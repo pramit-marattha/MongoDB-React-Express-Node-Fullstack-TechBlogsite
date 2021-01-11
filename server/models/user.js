68f83bd9bb9954275dd65a393c71d35dda22f36a
pramit-marattha <pramitmarattha5497@gmail.com>
2021-01-11 23:59:29 +0545

    salt hashed and shortid added

--- server/models/user.js
+++ server/models/user.js
@@ -62,13 +62,23 @@ UserSchema.virtual("password").set(function(password){
 });
 
 UserSchema.methods = {
+
+        authenticate: function(plainText){
+            return this.encryptPassword(plainText) === this.hashed_password;
+        },
+
+
     encryptPassword: function(password){
         if(!password) return ""
         try {
             return crypto.createHmac("sha1",this.salt).update(password).digest("hex")
         } catch(err) {
-
+            return ""
         }
+    },
+
+    makeSalt: function(){
+        return Math.round(new Date().valueOf() * Math.random()) + "";
     }
 }
 
