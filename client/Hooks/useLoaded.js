47f191ab129af960a84ead3f5e448b3b7a14cb4d
pramitmarattha <pramitmarattha5497@gmail.com>
2021-01-25 09:21:41 +0545

    Delete useLoaded.js

--- client/Hooks/useLoaded.js
+++
@@ -1,8 +0,0 @@
-import React,{useState,useEffect} from 'react'
-
-export const useLoaded = () => {
-    const [loaded, setLoaded] = useState(false);
-    useEffect(() => setLoaded(true), []);
-    return loaded;
-  };
-  
