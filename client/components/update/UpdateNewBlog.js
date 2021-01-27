198f6bd68769169b9c7fe77b3c3ba0c308623953
pramit-marattha <pramitmarattha5497@gmail.com>
2021-01-28 00:42:44 +0545

    populating the field with entire content on react quill

--- client/components/update/UpdateNewBlog.js
+++ client/components/update/UpdateNewBlog.js
@@ -7,12 +7,92 @@ import {withRouter} from "next/router"; // to get access to the router props fro
 import {getCookie,isAuthenticated} from "../../actions/authentication";
 import {getCategories} from "../../actions/category"; 
 import {getTagLists} from "../../actions/tag"; 
-import {createBlog} from "../../actions/blog";
+import {singleBlog,updatingTheBlog} from "../../actions/blog";
 import PublishIcon from '@material-ui/icons/Publish';
 import {ReactQuillModules,ReactQuillFormats} from "../../helpers/ReactQuill";
 
 
-const UpdateNewBlog = () =>{
+// dynamically importing react quill
+const ReactQuill = dynamic(() => import("react-quill"),{ssr:false}); 
+
+const UpdateNewBlog = ({router}) =>{
+    const [body,setBody] = useState('');
+    const [infos, setInfos] = useState({
+        title:'',
+        error: '',
+        success: '',
+        formData: '',
+        title: '',
+        body: ''
+    });
+
+    const {title, error, success, formData } = infos;
+
+
+    useEffect(()=>{
+        setInfos({ ...infos, formData: new FormData() });
+        initializeBlog();
+    },[router]);
+
+    const handleBody = e =>{
+        setBody(e)
+        formData.set('body',e)
+    };
+
+    const editingTheBlog = ()=>{
+        console.log('update')
+    };
+    
+    const initializeBlog =()=>{
+        if(router.query.slug){
+            singleBlog(router.query.slug).then(data=>{
+                if(data.error){
+                    console.log(data.error)
+                }else{
+                    setInfos({...infos,title:data.title});
+                    setBody(data.body);
+                }
+            })
+        }
+    };
+
+    const handleChange = name => event => {
+        // console.log(e.target.value);
+        const value = name === 'photo' ? event.target.files[0] : event.target.value;
+        formData.set(name, value);
+        setInfos({ ...infos, [name]: value, formData, error: '' });
+    };
+
+
+    const editingTheBlogForm = () => {
+        return (
+            <form onSubmit={editingTheBlog}>
+                <div className="form-group">
+                    <label className="text-muted">Title</label>
+                    <input type="text" className="form-control" value={title} onChange={handleChange('title')} placeholder="Please enter a title" />
+                </div>
+
+                <div className="form-group">
+                    <ReactQuill
+                        placeholder="Write your blog ♥"
+                        style={{backgroundColor:"white",color:"black"}}
+                        modules={ReactQuillModules}
+                        formats={ReactQuillFormats}
+                        value={body}
+                        onChange={handleBody}
+                    />
+                </div>
+
+                <div className="text-center">
+                    <button type="submit" className="btn btn-info pt-3 pb-3 pl-3 pr-3">
+                        Update it {" "}
+                        <PublishIcon/>
+                    </button>
+                </div>
+            </form>
+        );
+    };
+
     return (
         <div className="container-fluid">
             <div className="row">
@@ -42,7 +122,7 @@ const UpdateNewBlog = () =>{
                     </div>
                 </div>
                 <div className="col-md-8">
-                    Create Blog Form
+                    {editingTheBlogForm()}
                     <p>Show Sucess and error Message</p>
                     {/* {displayError()}
                     {displaySuccess()}
@@ -60,4 +140,4 @@ const UpdateNewBlog = () =>{
         </div>
     );
 }
-export default UpdateNewBlog;
+export default withRouter(UpdateNewBlog);
