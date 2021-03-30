93c84c25e7a9d35106aa651486e7a61e9cdee3d1
pramit-marattha <pramitmarattha5497@gmail.com>
2021-03-31 00:56:58 +0545

    category and tag admin dashboard update functionality

--- client/pages/adminDashboard/update/category-tag.js
+++ client/pages/adminDashboard/update/category-tag.js
@@ -3,35 +3,34 @@ import Admin from "../../../components/authentication/Admin";
 import Category from "../../../components/update/Category";
 import Tag from "../../../components/update/Tag";
 import Link from "next/link";
-import CategoryIcon from '@material-ui/icons/Category';
-import AddIcon from '@material-ui/icons/Add';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import CategoryIcon from "@material-ui/icons/Category";
+import AddIcon from "@material-ui/icons/Add";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 
-
-const CategoryTag =() =>{
-    return(
-        <Layout>
-        <Admin>
+const CategoryTag = () => {
+  return (
+    <Layout>
+      <Admin>
         <div className="container-fluid">
-        <div className="row">
-        <div className="col-md-12">
-        <h1 className="text-center">
-        <CategoryIcon style={{"color":"teal"}}/>
-        Create Catagories and Tags
-        <LocalOfferIcon style={{"color":"red"}}/>
-        </h1>
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="text-center">
+                <CategoryIcon style={{ color: "teal" }} />
+                Create Catagories and Tags
+                <LocalOfferIcon style={{ color: "red" }} />
+              </h1>
+            </div>
+            <div className="col-md-6 pt-5 pb-5">
+              <Category />
+            </div>
+            <div className="col-md-6 pt-5 pb-5">
+              <Tag />
+            </div>
+          </div>
         </div>
-        <div className="col-md-6 pt-5 pb-5">
-        <Category/>
-        </div>
-        <div className="col-md-6 pt-5 pb-5">
-            <Tag/>
-        </div>
-        </div>
-        </div>
-        </Admin>
-        </Layout>
-    )
-}
+      </Admin>
+    </Layout>
+  );
+};
 
 export default CategoryTag;
