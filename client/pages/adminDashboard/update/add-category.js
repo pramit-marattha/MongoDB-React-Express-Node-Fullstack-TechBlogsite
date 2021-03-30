e32c88213870336cc0feae7b753590321b9c5c66
pramit-marattha <pramitmarattha5497@gmail.com>
2021-03-31 00:39:56 +0545

    category and tag admin dashboard update functionality

---
+++ client/pages/adminDashboard/update/add-category.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const addUpdateCategory = () => {
+  return <div>Category update</div>;
+};
+
+export default addUpdateCategory;
---
+++ client/pages/adminDashboard/update/add-tag.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const addUpdateTags = () => {
+  return <div>Tag update</div>;
+};
+
+export default addUpdateTags;
